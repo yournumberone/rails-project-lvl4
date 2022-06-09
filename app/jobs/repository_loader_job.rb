42dd65af52a09677a51444e343f1507cf9427040
yournumberone <marsthegod@protonmail.com>
2022-06-10 01:27:31 +0300

    Update repository_loader_job.rb

--- app/jobs/repository_loader_job.rb
+++ app/jobs/repository_loader_job.rb
@@ -5,12 +5,16 @@ class RepositoryLoaderJob < ApplicationJob
 
   def perform(object_id)
     object = Repository.find_by(id: object_id)
+    puts '1*' * 80
     return if object.nil?
-
+    puts '2*' * 80
     str = "#{object.link}.git"
+    puts '3*' * 80
     begin
       Git.clone(str, "repositories/#{object.id}")
+      puts '4*' * 80
     rescue StandardError
+      puts '4.2*' * 80
       Rails.logger.debug 'oops'
     end
   end
