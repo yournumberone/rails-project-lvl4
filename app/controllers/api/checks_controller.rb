8a47a827580abd64bbab26a82cfe3e8fe33ced7a
yournumberone <marsthegod@protonmail.com>
2022-06-22 10:29:40 +0300

    Update checks_controller.rb

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -2,13 +2,13 @@
 
 class Api::ChecksController < Api::ApplicationController
   def create
-    return unless params.key?('ref')
+    if params.key?('ref')
+      repository = Repository.find_by(github_id: params['repository']['id'])
+      return if repository.nil?
 
-    repository = Repository.find_by(github_id: params['repository']['id'])
-    return if repository.nil?
-
-    check = repository.checks.new
-    LintRepositoryJob.perform_later(check.id) if check.save
-    render json: { '200': 'I don\'t wanna flirt with you, but I really wanna see a green pipeline' }, status: :ok
+      check = repository.checks.new
+      LintRepositoryJob.perform_later(check.id) if check.save
+    end
+    render json: { '200': 'OK' }, status: :ok
   end
 end
