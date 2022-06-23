a52dab9838eff3dbf71bdf5597335a336b23f727
yournumberone <marsthegod@protonmail.com>
2022-06-23 16:08:10 +0300

    Update lint_repository_job.rb

--- app/jobs/lint_repository_job.rb
+++ app/jobs/lint_repository_job.rb
@@ -7,23 +7,25 @@ class LintRepositoryJob < ApplicationJob
     check = Repository::Check.find_by(id: check_id)
     return if check.nil?
 
+    puts check.repository.inspect
+    puts '*-' * 90
     repository = check.repository
 
     check.to_checking!
 
-    # begin
-    ApplicationContainer[:load_repository].download(repository.id)
-    check.result = ApplicationContainer[:linter].check(repository.language, repository.id)
-    case repository.language
-    when 'Ruby'
-      check.passed = JSON.parse(check.result)['summary']['offense_count'].zero?
-    when 'JavaScript'
-      check.passed = JSON.parse(check.result).empty?
+    begin
+      ApplicationContainer[:load_repository].download(repository.id)
+      check.result = ApplicationContainer[:linter].check(repository.language, repository.id)
+      case repository.language
+      when 'Ruby'
+        check.passed = JSON.parse(check.result)['summary']['offense_count'].zero?
+      when 'JavaScript'
+        check.passed = JSON.parse(check.result).empty?
+      end
+      check.finish!
+    rescue StandardError
+      check.fail!
     end
-    check.finish!
-    # rescue StandardError
-    #   check.fail!
-    # end
     check.send_results_email unless check.passed?
   end
 end
