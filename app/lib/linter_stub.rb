c4936f05d593efd9c4014021f546a30db9f00718
yournumberone <marsthegod@protonmail.com>
2022-06-23 15:54:52 +0300

    test without exceptions rescue

--- app/lib/linter_stub.rb
+++ app/lib/linter_stub.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-# not intended for Github Actions
 class LinterStub
   def self.check(language, _id)
     File.read(Rails.root.join("test/fixtures/files/#{language}/result.json"))
